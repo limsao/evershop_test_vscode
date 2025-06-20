988379170af852954127fc80d40cf27c97c2d9aa
salim <s.lalla@it-students.fr>
2025-06-20 16:08:04 +0200

    Vendredi20/06 16h

--- test_faire_argent.py
+++ test_faire_argent.py
@@ -10,8 +10,8 @@ import time
 
 driver= webdriver.Chrome(service=chromeservice(ChromeDriverManager().install()))
 
-driver.get( "http://localhost:3000/classic-leather-loafers" )
-
+driver.get( "http://localhost:3000/" )
+driver.find_element(By.LINK_TEXT, "Classic Leather Loafers").click()
 
 
 driver.find_element(By.CSS_SELECTOR, ".button.primary.outline").click()
@@ -25,7 +25,7 @@ WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.CSS_SELECTOR, ".checkout-payment.checkout-step"))
     )
 
-# on rempli le form de shipping
+# on rempli le form d e shipping
 driver.find_element(By.NAME, "address[full_name]").send_keys("Nom prenom")
 driver.find_element(By.NAME, "address[telephone]").send_keys("0607080911")
 driver.find_element(By.NAME, "address[address_1]").send_keys("rue des fruits")
@@ -34,33 +34,10 @@ driver.find_element(By.NAME, "address[postcode]").send_keys("69999")
 driver.find_element(By.NAME, "address[telephone]").send_keys("0607080911")
 Select(driver.find_element(By.ID, "address[country]")).select_by_index(1)
 Select(driver.find_element(By.ID, "address[province]")).select_by_index(1)
-WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "label[for='method0']"))).click()
-driver.find_element(By.CSS_SELECTOR, "button[type='submit'].primary").click()
-WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.CSS_SELECTOR, "div.flex.justify-start.items-center.gap-4 a"))
-).click()
-# Attendre que l’iframe Stripe soit là
-iframe = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "iframe[name^='__privateStripeFrame']"))
-)
-
-# on passe dans l’iframe
-driver.switch_to.frame(iframe)
-
-WebDriverWait(driver, 10).until(
-    EC.visibility_of_element_located((By.NAME, "number"))  # parfois "number" selon le nom attribué
-)
-
-# Remplir la carte
-driver.find_element(By.NAME, "number").send_keys("4242424242424242")
-driver.find_element(By.NAME, "expiry").send_keys("0426")
-driver.find_element(By.NAME, "cvc").send_keys("242")
-
-# on ressort de l'iframe
-driver.switch_to.default_content()
-
-#on fait l'argent
-driver.find_element(By.CSS_SELECTOR, ".form-submit-button").find_element(By.CSS_SELECTOR, ".button.primary").click()
+time.sleep(3)
+driver.find_element(By.CSS_SELECTOR, "input[type='radio'][value='e6970240-54c0-43f5-8b0f-ea3213735f75']").click()
+#driver.find_element(By.CSS_SELECTOR, ".field-wrapper.radio-field").find_element(By.CSS_SELECTOR, ".method0").click()
 time.sleep(12)
+#driver.find_element(By.CSS_SELECTOR, ".form-submit-button.flex.border-t.border-divider.mt-4.pt-4").find_element(By.CSS_SELECTOR, ".button.primary").click()
 
-#faut verifier l'argent
+#ckjbckjd
